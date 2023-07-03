c07776f6035c116ee6f6a480666e7ea5d55c82a8
alan890104 <alan890104@gmail.com>
2023-07-03 23:21:04 +0800

    * chore: add .env.example file * * chore: add .prettierignore file * * feat: add Dockerfile for containerization * * feat: add nginx.conf for nginx configuration * * refactor(vite.config.ts): update vite.config.ts to include alias for '@'

---
+++ Dockerfile
@@ -0,0 +1,11 @@
+FROM node:lts-alpine as build-stage
+RUN mkdir -p /app
+WORKDIR /app
+COPY package*.json ./
+RUN yarn && yarn build
+
+FROM nginx:stable-alpine as production-stage
+COPY nginx.conf /etc/nginx/conf.d/default.conf
+RUN rm -rf /usr/share/nginx/html/*
+COPY --from=build-stage /app/dist /usr/share/nginx/html
+CMD ["nginx", "-g", "daemon off;"]
--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,17 @@
-import { defineConfig } from 'vite'
-import { fileURLToPath, URL } from 'node:url'
+import { defineConfig } from 'vite';
+import { fileURLToPath, URL } from 'node:url';
 
-import vue from '@vitejs/plugin-vue'
+import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
+
+// https://blog.csdn.net/qq_43519131/article/details/127408209
+
 export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+	plugins: [vue()],
+	resolve: {
+		alias: {
+			'@': fileURLToPath(new URL('./src', import.meta.url)),
+		},
+	},
+});
