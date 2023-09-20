a5741bccd34daf5e9aac75f6f22b6df46de1f7e6
WaterSo0910 <jacky950141@gmail.com>
2023-09-21 00:02:02 +0800

    Refactor base path configuration in vite.config.ts
    The previous base path configuration has been refactored to set the base path conditionally based on the environment variable VITE_APP_MODE. The new configuration sets the base path to '/shin-feng-frontend' when VITE_APP_MODE is 'demo'. This change improves flexibility and ensures the correct base path is used in the application.

--- vite.config.ts
+++ vite.config.ts
@@ -4,13 +4,10 @@ import { fileURLToPath, URL } from 'node:url';
 import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
-
 // https://blog.csdn.net/qq_43519131/article/details/127408209
-
 export default defineConfig({
 	plugins: [vue()],
-	root: './',
-	base: process.env.VITE_APP_MODE !== 'staging' ? '/shin-feng-frontend' : '/',
+	base: process.env.VITE_APP_MODE === 'demo' ? '/shin-feng-frontend' : '/',
 	resolve: {
 		alias: {
 			'@': fileURLToPath(new URL('./src', import.meta.url)),
