210dbe9db8ada955790f04160aee5dd0d3630d53
WaterSo0910 <jacky950141@gmail.com>
2023-09-21 13:47:50 +0800

    Fix base URL for demo mode in vite.config.ts
    The base URL in vite.config.ts has been updated to include a trailing slash in demo mode to ensure correct routing. This change fixes an issue where the app was not loading assets correctly in demo mode.

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import vue from '@vitejs/plugin-vue';
 // https://blog.csdn.net/qq_43519131/article/details/127408209
 export default defineConfig({
 	plugins: [vue()],
-	base: process.env.VITE_APP_MODE === 'demo' ? '/shin-feng-frontend' : '/',
+	base: process.env.VITE_APP_MODE === 'demo' ? '/shin-feng-frontend/' : '/',
 	resolve: {
 		alias: {
 			'@': fileURLToPath(new URL('./src', import.meta.url)),
